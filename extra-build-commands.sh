09eadc812f62147dabf4833007e0a0e19bdef899
Joe Crim <josephcrim@gmail.com>
2014-01-29 10:23:53 -0800

    Working webrockit-ui into a build system
    I want to start moving towards packaging this software with rpm and
    versioning it later on. These bits will help us move towards that model
    and away from git cloning to install.

---
+++ extra-build-commands.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# include any additional steps that need to be followed, just prior to building the rpm here
+# this script will be run from the target build target path
+
+PROBLEMS=0
+OLDPWD=`pwd`
+
+mkdir ./etc/init.d
+mv ./opt/webrockit-ui/ext/webrockit-ui.init ./etc/init.d/webrockit-ui
+chmod 755 ./etc/init.d/webrockit-ui
+if [ $? -ne 0 ]
+then
+    PROBLEMS=99
+fi
+cd ${OLDPWD}
+
+if [ ${PROBLEMS} -ne 0 ]
+then
+    exit 1
+else
+    exit 0
+fi
