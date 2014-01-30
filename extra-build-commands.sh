d54023ede164656e41adcacc1e637260a596a20f
Joe Crim <josephcrim@gmail.com>
2014-01-30 10:39:23 -0800

    Allow jenkins to set the revision with the build number for now.

--- extra-build-commands.sh
+++ extra-build-commands.sh
@@ -9,7 +9,9 @@ OLDPWD=`pwd`
 mkdir -p ./etc/init.d
 mv ./opt/webrockit-ui/ext/webrockit-ui.init ./etc/init.d/webrockit-ui
 chmod 755 ./etc/init.d/webrockit-ui
-mv ./opt/webrockit-ui/config/config.yml.example ./opt/webrockit-ui/config/config.yml
+cp ./opt/webrockit-ui/config/config.yml.example .
+rm -rf ./opt/webrockit-ui/config/*
+mv config.yml.example ./opt/webrockit-ui/config/config.yml
 
 if [ $? -ne 0 ]
 then
